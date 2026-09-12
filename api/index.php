b0363d940180074b726cfbc238246a98c840bf96
Faishal <ninikmasruroh884@gmail.com>
2026-09-12 08:40:55 +0700

    Fix Laravel cache on Vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,13 @@
 <?php
 
+$tmpViews = '/tmp/laravel/views';
+
+if (!is_dir($tmpViews)) {
+    mkdir($tmpViews, 0777, true);
+}
+
+putenv("VIEW_COMPILED_PATH={$tmpViews}");
+$_ENV['VIEW_COMPILED_PATH'] = $tmpViews;
+$_SERVER['VIEW_COMPILED_PATH'] = $tmpViews;
+
 require __DIR__ . '/../public/index.php';
